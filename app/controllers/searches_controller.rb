8304572c8b587fb8c4ce69004bdada853bad090c
sophie liu <sophieliu@hotmail.fr>
2018-09-30 11:04:01 +0200

    added the form for search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,11 +1,21 @@
 class SearchesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:new, :create]
 
   def new
-
+    @search = Search.new
   end
 
   def create
+    @search = Search.new(search_params)
+    @search.save
+    redirect_to search_path(@restaurant)
+  end
 
+
+  private
+
+  def search_params
+    params.require(:search).permit(:content)
   end
 
 end
